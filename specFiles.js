7003b3b5ba9474cc36ed77b0ad02b3f5bbcb0c1c
Lazared <eduard.enl@gmail.com>
2024-09-18 14:43:56 +0100

    index.html spec changes

--- specFiles.js
+++ specFiles.js
@@ -1,4 +1,5 @@
 // include in this array all Shamrock Booking As A Service API's/services spec.yaml URLs.
 const specFiles = [
-  'https://raw.githubusercontent.com/addleeplc/shamrock-services-oas/master/shamrock-booking-cache-v2/spec.yaml?token=GHSAT0AAAAAACUM6ID6ZSIYNURR4ZYTBM6OZXKY3JQ'
+  'https://raw.githubusercontent.com/addleeplc/baas-oas-api/main/oas/shamrock-booking-cache-v2/spec.yaml',
+  'https://raw.githubusercontent.com/addleeplc/baas-oas-api/main/oas/shamrock-booking-service-v1/spec.yaml'
 ];
