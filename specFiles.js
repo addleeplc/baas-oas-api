9a4c962fad3eefd80ea4a2fe28e3d3362f6403fa
Lazared <eduard.enl@gmail.com>
2024-09-18 14:51:34 +0100

    relative path for specFiles

--- specFiles.js
+++ specFiles.js
@@ -1,5 +1,5 @@
 // include in this array all Shamrock Booking As A Service API's/services spec.yaml URLs.
 const specFiles = [
-  'https://raw.githubusercontent.com/addleeplc/baas-oas-api/main/oas/shamrock-booking-cache-v2/spec.yaml',
-  'https://raw.githubusercontent.com/addleeplc/baas-oas-api/main/oas/shamrock-booking-service-v1/spec.yaml'
+  './oas/shamrock-booking-cache-v2/spec.yaml',
+  './oas/shamrock-booking-service-v1/spec.yaml'
 ];
